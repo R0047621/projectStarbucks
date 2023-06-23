30537bfdf61edf5763090a623c602745a552a666
xin <1135422782@qq.com>
2023-06-23 21:47:55 +0800

    feat(more): 添加了更多和底部
    111
    
    BREAKING CHANGE:
    111
    
    ISSUES CLOSED: 11

--- src/views/more/more.js
+++ src/views/more/more.js
@@ -2,5 +2,5 @@ let svgNode = document.querySelector('.icon');
 console.log(svgNode);
 
 svgNode.addEventListener('click',function(){
-    location.href = '../home/home.html';
+    window.history.go(-1);
 })
